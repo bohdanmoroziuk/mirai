263948a78b8eae2d831c62c3d4213e229597dc3d
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-07-20 20:48:06 +0300

    feat(notes): add a list of topics

---
+++ src/modules/notes/components/TopicList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="topic-list">
+    <q-chip
+      class="topic-list-item"
+      v-for="topic of topics"
+      :key="topic.id"
+      color="primary"
+      text-color="white"
+    >
+      {{ topic.name }}
+    </q-chip>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+import { Topic } from 'src/modules/notes/stores/topics';
+
+interface Props {
+  topics: Topic[];
+}
+
+defineProps<Props>();
+</script>
