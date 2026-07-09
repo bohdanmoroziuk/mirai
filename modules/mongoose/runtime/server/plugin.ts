7ce56d0bc9820d168af69a9c4230b45325151c65
Bohdan Moroziuk <bogdan7morozyuk@gmail.com>
2026-07-09 13:01:24 +0300

    fix(mongoose): improve connection lifecycle

--- modules/mongoose/runtime/server/plugin.ts
+++ modules/mongoose/runtime/server/plugin.ts
@@ -1,8 +1,12 @@
 import { requireMongodbUri, connectMongoose, disconnectMongoose } from './mongoose'
 
 export default defineNitroPlugin((nitroApp) => {
+  const mongodbUri = requireMongodbUri()
+
+  void connectMongoose(mongodbUri).catch(() => undefined)
+
   nitroApp.hooks.hook('request', async () => {
-    await connectMongoose(requireMongodbUri())
+    await connectMongoose(mongodbUri)
   })
 
   nitroApp.hooks.hook('close', async () => {
